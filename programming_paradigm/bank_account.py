972edaaf6a24c1c052fa520a0b20e73d92a420ac
Emma Karimi <emmaannkinyua@gmail.com>
2025-02-01 22:43:15 +0300

    Fixed display_balance formatting

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,6 +17,7 @@ class BankAccount:
         return True  # Successful withdrawal
     
     def display_balance(self):
-        """"Print the current balance."""
-        print(f"Current Balance: ${self.account_balance}")
+        """Print the current balance with two decimal places."""
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
          
